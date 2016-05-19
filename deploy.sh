a4df4ea03d90edbf95e5e31e07d302f9c6265417
shunfei.wan <shunfei.wan@qunar.com>
2016-05-19 14:16:05 +0800

    s

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if test $1 = 'test'
+then
+  hexo clean
+  hexo g && gulp
+  hexo s
+else
+  hexo clean
+  hexo g && gulp
+  hexo deploy
+  hexo clean
+  echo 'adding files...'
+  gaa
+  echo 'commiting files...'
+  gcam 'commit change files'
+  echo 'pushing to remote branch hexo_blog_code...'
+  git push origin hexo_blog_code
+fi
